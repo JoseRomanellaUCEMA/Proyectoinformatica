4163c9841f4ec56e8baf8727a93ea449673063ea
JoseRomanellaUCEMA <jromanell26@ucema.edu.ar>
2023-10-26 11:55:22 -0300

    Se crea base de datos

--- database.py
+++ database.py
@@ -1,2 +1,33 @@
 import sqlite3
 
+conn = sqlite3.connect('concesionaria.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS autos (
+        VIN TEXT PRIMARY KEY,
+        make TEXT,
+        model TEXT,
+        year INTEGER,
+        price REAL,
+        color TEXT,
+        mileage REAL,
+        condition TEXT,
+        features TEXT
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS ventas (
+        ID INTEGER PRIMARY KEY,
+        VIN TEXT,
+        date TEXT,
+        price REAL,
+        buyer TEXT,
+        FOREIGN KEY (VIN) REFERENCES autos (VIN)
+    )
+''')
+
+conn.commit()
+
+conn.close()
---
+++ product.py
@@ -0,0 +1,35 @@
+class Auto:
+
+    def __init__(self, VIN, make, model, year, price, color, mileage, condition, features) -> None:
+        self.VIN = VIN
+        self.make = make
+        self.model = model
+        self.year = year
+        self.price = price
+        self.color = color
+        self.mileage = mileage
+        self.condition = condition
+        self.features = features
+
+    def serialize(self):
+        return {
+            'VIN': self.VIN,
+            'make': self.make,
+            'model': self.model,
+            'year': self.year,
+            'price': self.price
+        }
+
+    def serialize_details(self):
+        return {
+            'VIN': self.VIN,
+            'make': self.make,
+            'model': self.model,
+            'year': self.year,
+            'price': self.price,
+            'color': self.color,
+            'mileage': self.mileage,
+            'condition': self.condition,
+            'features': self.features
+        }
+
