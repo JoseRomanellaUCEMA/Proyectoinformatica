49e64745ccfad2cc1f37d632e8717001e86dbc9a
JoseRomanellaUCEMA <jromanell26@ucema.edu.ar>
2023-10-30 15:26:38 -0300

    Modificacion de api_routes
    se utilizan las clases Auto y Venta para crear instancias de objetos y serializarlos adecuadamente en las respuestas JSON.

--- api_routes.py
+++ api_routes.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from product import Auto
+from sale import Venta
 import sqlite3
 
 app = Flask(__name__)
@@ -10,8 +12,14 @@ def get_autos():
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM autos')
-    autos = cursor.fetchall()
+    autos_data = cursor.fetchall()
     conn.close()
+    
+    autos = []
+    for auto_data in autos_data:
+        auto = Auto(*auto_data)
+        autos.append(auto.serialize_details())
+    
     return jsonify(autos)
 
 @app.route('/autos', methods=['POST'])
@@ -21,11 +29,13 @@ def add_auto():
     if 'VIN' not in data or 'make' not in data or 'model' not in data or 'year' not in data or 'price' not in data or 'color' not in data or 'mileage' not in data or 'condition' not in data or 'features' not in data:
         return jsonify({'error': 'Faltan campos obligatorios'}), 400
 
+    auto = Auto(data['VIN'], data['make'], data['model'], data['year'], data['price'], data['color'], data['mileage'], data['condition'], data['features'])
+
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     try:
         cursor.execute('INSERT INTO autos (VIN, make, model, year, price, color, mileage, condition, features) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                       (data['VIN'], data['make'], data['model'], data['year'], data['price'], data['color'], data['mileage'], data['condition'], data['features']))
+                       (auto.VIN, auto.make, auto.model, auto.year, auto.price, auto.color, auto.mileage, auto.condition, auto.features))
         conn.commit()
         conn.close()
 
@@ -39,10 +49,12 @@ def get_auto(vin):
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM autos WHERE VIN = ?', (vin,))
-    auto = cursor.fetchone()
+    auto_data = cursor.fetchone()
     conn.close()
-    if auto:
-        return jsonify(auto)
+    
+    if auto_data:
+        auto = Auto(*auto_data)
+        return jsonify(auto.serialize_details())
     else:
         return jsonify({'error': 'Auto no encontrado'}), 404
 
@@ -53,10 +65,12 @@ def update_auto(vin):
     if 'make' not in data or 'model' not in data or 'year' not in data or 'price' not in data or 'color' not in data or 'mileage' not in data or 'condition' not in data or 'features' not in data:
         return jsonify({'error': 'Faltan campos obligatorios'}), 400
 
+    auto = Auto(vin, data['make'], data['model'], data['year'], data['price'], data['color'], data['mileage'], data['condition'], data['features'])
+
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('UPDATE autos SET make = ?, model = ?, year = ?, price = ?, color = ?, mileage = ?, condition = ?, features = ? WHERE VIN = ?',
-                   (data['make'], data['model'], data['year'], data['price'], data['color'], data['mileage'], data['condition'], data['features'], vin))
+                   (auto.make, auto.model, auto.year, auto.price, auto.color, auto.mileage, auto.condition, auto.features, vin))
     conn.commit()
     conn.close()
     return jsonify({'message': 'Auto actualizado exitosamente'})
@@ -76,8 +90,14 @@ def get_autos_by_make():
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM autos WHERE make = ?', (marca,))
-    autos = cursor.fetchall()
+    autos_data = cursor.fetchall()
     conn.close()
+    
+    autos = []
+    for auto_data in autos_data:
+        auto = Auto(*auto_data)
+        autos.append(auto.serialize_details())
+    
     if not autos:
         return jsonify({'error': 'No se encontraron autos de la marca especificada'}), 404
     return jsonify(autos)
@@ -87,8 +107,14 @@ def get_ventas():
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM ventas')
-    ventas = cursor.fetchall()
+    ventas_data = cursor.fetchall()
     conn.close()
+    
+    ventas = []
+    for venta_data in ventas_data:
+        venta = Venta(*venta_data)
+        ventas.append(venta.serialize())
+    
     return jsonify(ventas)
 
 @app.route('/ventas', methods=['POST'])
@@ -98,10 +124,12 @@ def add_venta():
     if 'VIN' not in data or 'date' not in data or 'price' not in data or 'make' not in data or 'buyer' not in data:
         return jsonify({'error': 'Faltan campos obligatorios'}), 400
 
+    venta = Venta(None, data['VIN'], data['date'], data['price'], data['make'], data['buyer'])
+
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('INSERT INTO ventas (VIN, date, price, make, buyer) VALUES (?, ?, ?, ?, ?)',
-                   (data['VIN'], data['date'], data['price'], data['make'], data['buyer']))
+                   (venta.VIN, venta.date, venta.price, venta.make, venta.buyer))
     conn.commit()
     conn.close()
 
@@ -113,13 +141,18 @@ def get_ventas_by_make():
     conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM ventas WHERE make = ?', (marca,))
-    autos = cursor.fetchall()
+    ventas_data = cursor.fetchall()
     conn.close()
-    if not autos:
+    
+    ventas = []
+    for venta_data in ventas_data:
+        venta = Venta(*venta_data)
+        ventas.append(venta.serialize())
+    
+    if not ventas:
         return jsonify({'error': 'No se encontraron ventas de autos de la marca especificada'}), 404
-    return jsonify(autos)
-
-
+    return jsonify(ventas)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
